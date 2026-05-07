ca95641b5d1da191f07cc35ac2fcba5e7368a925
Casey Waweru <casey.waweru@gmail.com>
2026-05-08 00:50:32 +0300

    refactor: moving the projects object array to the parent component of the project

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,22 +1,4 @@
 function ProjectList() {
-  const projects = [
-    {
-      id: 1,
-      title: "Project 1",
-      description: "A Clock App",
-    },
-    {
-      id: 2,
-      title: "Project 2",
-      description: "A Project List Display App",
-    },
-    {
-      id: 3,
-      title: "Project 3",
-      description: "A Spotify Playlist Organiser",
-    },
-  ]
-
   return (
   )
 }
