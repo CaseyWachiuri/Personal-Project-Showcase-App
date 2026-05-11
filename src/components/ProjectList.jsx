ae13cea32b015fe392032ab7c9a2ba8f838ced25
Casey Waweru <casey.waweru@gmail.com>
2026-05-11 15:25:08 +0300

    style: adding slight styling to the projects list

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -3,8 +3,17 @@ import ProjectItem from './ProjectItem'
 function ProjectList({ projects }) {
   const projectItems = projects.map((proj) => (<ProjectItem key={proj.id} title={proj.title} description={proj.description} />))
 
+  function handleSearch() {
+    console.log("Hello there");
+  }
+
   return (
-    <div className="border bg-red=800 rounded flex justify-center h6 items-center m-2">
+    <div className="border bg-red=800 rounded flex-col justify-center h6 items-center m-2">
+      <input className="border flex"
+        type='search'
+        value={name}
+        onChange={handleSearch}
+        placeholder="search projects" />
       <ul>
         {projectItems}
       </ul>
