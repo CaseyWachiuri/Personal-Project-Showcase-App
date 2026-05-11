0991424f87a4ebd6e73efe31c6ac405b2deb60d7
Casey Waweru <casey.waweru@gmail.com>
2026-05-11 17:06:56 +0300

    feat: handling form submission

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -5,20 +5,41 @@ function AddProject() {
   const [description, setDescription] = useState('');
 
   // Handle the submitted form data
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("form submitted successfully");
   }
+
+  function handleTitleChange(e) {
+    setTitle(e.target.value)
+  }
+
+  console.log(title);
+
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value)
+  }
+
+  console.log(description);
 
   return (
     <>
       <form className="border bg-red=800 rounded flex-col justify-center h6 items-center m-2"
-        onSubmit={handleSubmit}>
+        onSubmit={handleSubmit}
+      >
         <div className="">
           <label>Title</label>
-          <input name="Title" />
+          <input
+            type="text"
+            onChange={handleTitleChange}
+          />
         </div>
         <div className="">
           <label>Description</label>
-          <input name="Description" />
+          <input
+            type="text"
+            onChange={handleDescriptionChange}
+          />
         </div>
         <button type="submit">Add</button>
       </form>
