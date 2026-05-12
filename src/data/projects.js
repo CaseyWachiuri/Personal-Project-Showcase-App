41a0712f8e7370999d3240cdab4d38929942bec6
Casey Waweru <casey.waweru@gmail.com>
2026-05-12 03:57:00 +0300

    chore: renaming the kv pairs for the data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 const data = [
-  { id: 1, title: "Project 1", description: "A Clock App" },
-  { id: 2, title: "Project 2", description: "A Project List Display App" },
-  { id: 3, title: "Project 3", description: "A Spotify Playlist Organiser" },
+  { id: 1, title: "Clock App", description: "A Simple Clock App that tells the current time" },
+  { id: 2, title: "Project Showcase App", description: "A Simple App with a list of all the projects I've done" },
+  { id: 3, title: "A Spotify Playlist Organiser", description: "An app to organize your spotify playlists and remove redundant songs" },
 ];
 
 export default data;
