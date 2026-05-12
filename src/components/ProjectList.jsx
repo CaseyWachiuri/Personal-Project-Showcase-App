5f084e5eeee5309fcc865c163b0f987a65dcddc5
Casey Waweru <casey.waweru@gmail.com>
2026-05-12 03:55:20 +0300

    feat: integrating the project search functionality

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react';
 import ProjectItem from './ProjectItem'
 
 function ProjectList({ projects }) {
-  const projectItems = projects.map((proj) => (<ProjectItem key={proj.id} title={proj.title} description={proj.description} />))
-
   const [search, setSearch] = useState('')
+
+  // Filter Implementatoin. Look for another function in the future
+  const projectsToDisplay = projects.filter((proj) => (proj.title.toLowerCase().includes(search.toLowerCase())))
+
+  // Map through the filtered projects (displays all if nothing is typed
+  const projectItems = projectsToDisplay.map((proj) => (<ProjectItem key={proj.id} title={proj.title} description={proj.description} />))
 
   function handleSearchChange(e) {
     setSearch(e.target.value)
   }
 
-  console.log(search)
+  // Trial run implementation for the search bar
 
   return (
     <div className="border bg-gray-100 rounded-l flex flex-col justify-around m-2 p-2">
